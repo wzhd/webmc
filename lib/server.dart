3db6658625d157fe8cb42004ddd3bd7a8d36480c
Zihao Wang <wzhdev@gmail.com>
2014-10-22 16:15:11 +0800

    Use relative url

--- lib/server.dart
+++ lib/server.dart
@@ -11,10 +11,8 @@ final playUrl = new UrlPattern(r'/play');
 final thumbUrl = new UrlPattern(r'/thumb');
 String mediaDir = '/home/wzhd/Videos/';
 String thumbDir = '/home/wzhd/.thumbnails/normal/';
-String hostAddr = '127.0.0.1:8000';
 
 serveIndex(HttpRequest req) {
-  hostAddr = req.headers.host + ':' + req.headers.port.toString();
 
   req.response.headers.contentType = ContentType.HTML;
   req.response.write('''<!DOCTYPE html>
@@ -32,8 +30,8 @@ serveIndex(HttpRequest req) {
   new Directory(mediaDir).list().listen((FileSystemEntity entity) {
     try {
       req.response.write(
-          '''<img src="http://$hostAddr/thumb?filename=${entity.path}"/><br>
-              <a href="http://$hostAddr/play?filename=${entity.path}">
+          '''<img src="/thumb?filename=${entity.path}"/><br>
+              <a href="/play?filename=${entity.path}">
               ${entity.path}
               </a><br>''');
     } catch (exception, stackTrace) {
