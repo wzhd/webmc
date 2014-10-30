fe0fdc940c1a7fab859725844ecf7532f794f6e5
Zihao Wang <wzhdev@gmail.com>
2014-10-30 20:55:54 +0800

    custom element programme-card

---
+++ lib/src/client/components/programme_card/programme_card.dart
@@ -0,0 +1,10 @@
+library webmc.programme_card;
+
+import 'package:polymer/polymer.dart';
+
+@CustomTag('programme-card')
+class ProgrammeCard extends PolymerElement {
+
+  ProgrammeCard.created() : super.created();
+
+}
